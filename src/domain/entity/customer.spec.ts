8303df71eb0a1e63adabfcbc434d90f2e3203f86
José Antônio <joseantonio1@hotmail.com.br>
2024-01-03 12:13:52 -0300

    fix: customer class and tests

--- src/domain/entity/customer.spec.ts
+++ src/domain/entity/customer.spec.ts
@@ -1,19 +1,19 @@
-import { Customer } from "./customer";
+import Customer from "./customer";
 
 describe('Customer unity tests', () => {
   it('should create a customer', () => {
-    const customer = new Customer('1', 'John Doe', 'Rua 1');
+    const customer = new Customer('1', 'John Doe');
     expect(customer).toBeTruthy();
   });
 
   it('should throw an error when id is empty', () => {
     expect(() => {
-        const customer = new Customer('', 'Jose', 'aaaaa, 32');
+        const customer = new Customer('', 'Jose');
     }).toThrow('Id is required');
   });
 
   it('should change customer name', () => {
-    const customer = new Customer('1', 'John Doe', 'Rua 1');
+    const customer = new Customer('1', 'John Doe');
     customer.changeName('Jose');
     expect(customer.name).toBe('Jose');
   });
